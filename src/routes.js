f1d9a921f2b7996087646e7b3f1ffe61208683f6
Muhammad Fajar <mhddfajar@gmail.com>
2022-08-04 08:37:13 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { handler } = require('@hapi/hapi/lib/cors');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+const route = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    }
+];
+
+module.exports = route;
